066fb1c1cf61e8079b20c4aeacdc40145f26f6b3
invisul <54588758+invisul@users.noreply.github.com>
2021-12-19 12:50:54 +0200

    assigment given files

---
+++ SoftSVM.py
@@ -0,0 +1,137 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+import numpy as np
+
+class SoftSVM(BaseEstimator, ClassifierMixin):
+    """
+    Custom C-Support Vector Classification.
+    """
+    def __init__(self, C: float, lr: float = 1e-5):
+        """
+        Initialize an instance of this class.
+        ** Do not edit this method **
+
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param lr: the SGD learning rate (step size)
+        """
+        self.C = C
+        self.lr = lr
+        self.batch_size = 32
+        self.w = None
+        self.b = 0.0
+
+    # Initialize a random weight vector
+    def init_solution(self, n_features: int):
+        """
+        Randomize an initial solution (weight vector)
+        ** Do not edit this method **
+
+        :param n_features:
+        """
+        self.w = np.random.randn(n_features)
+        self.b = 0.0
+
+    @staticmethod
+    def loss(w, b: float, C: float, X, y):
+        """
+        Compute the SVM objective loss.
+
+        :param w: weight vector for linear classification; array of shape (n_features,)
+        :param b: bias scalar for linear classification
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param X: samples for loss computation; array of shape (n_samples, n_features)
+        :param y: targets for loss computation; array of shape (n_samples,)
+        :return: the Soft SVM objective loss (float scalar)
+        """
+        margins = (X.dot(w) + b).reshape(-1, 1)
+        hinge_inputs = np.multiply(margins, y.reshape(-1, 1))
+
+        norm = np.linalg.norm(w)
+
+        # TODO: complete the loss calculation
+        loss = 0.0
+
+        return
+
+    @staticmethod
+    def subgradient(w, b: float, C: float, X, y):
+        """
+        Compute the (analytical) SVM objective sub-gradient.
+
+        :param w: weight vector for linear classification; array of shape (n_features,)
+        :param b: bias scalar for linear classification
+        :param C: inverse strength of regularization. Must be strictly positive.
+        :param X: samples for loss computation; array of shape (n_samples, n_features)
+        :param y: targets for loss computation; array of shape (n_samples,)
+        :return: a tuple with (the gradient of the weights, the gradient of the bias)
+        """
+        # TODO: calculate the analytical sub-gradient of soft-SVM w.r.t w and b
+        g_w = None
+        g_b = 0.0
+
+        return g_w, g_b
+
+    def fit_with_logs(self, X, y, max_iter: int = 2000, keep_losses: bool = True):
+        """
+        Fit the model according to the given training data.
+
+        :param X: training samples; array of shape (n_samples, n_features)
+        :param y: training targets; array of shape (n_samples,)
+        :param max_iter: number of SGD iterations
+        :param keep_losses:
+        :return: the training losses and accuracies during training
+        """
+        # Initialize learned parameters
+        self.init_solution(X.shape[1])
+
+        losses = []
+        accuracies = []
+
+        if keep_losses:
+            losses.append(self.loss(self.w, self.b, self.C, X, y))
+            accuracies.append(self.score(X, y))
+
+        # Iterate over batches
+        for iter in range(0, max_iter):
+            start_idx = (iter * self.batch_size) % X.shape[0]
+            end_idx = min(X.shape[0], start_idx + self.batch_size)
+            batch_X = X[start_idx: end_idx]
+            batch_y = y[start_idx: end_idx]
+
+            # TODO: Compute the (sub)gradient of the current *batch*
+            g_w, g_b = None, None
+
+            # Perform a (sub)gradient step
+            # TODO: update the learned parameters correctly
+            self.w = None
+            self.b = 0.0
+
+            if keep_losses:
+                losses.append(self.loss(self.w, self.b, self.C, X, y))
+                accuracies.append(self.score(X, y))
+
+        return losses, accuracies
+
+    def fit(self, X, y, max_iter: int = 2000):
+        """
+        Fit the model according to the given training data.
+        ** Do not edit this method **
+
+        :param X: training samples; array of shape (n_samples, n_features)
+        :param y: training targets; array of shape (n_samples,)
+        :param max_iter: number of SGD iterations
+        """
+        self.fit_with_logs(X, y, max_iter=max_iter, keep_losses=False)
+
+        return self
+
+    def predict(self, X):
+        """
+        Perform classification on samples in X.
+
+        :param X: samples for prediction; array of shape (n_samples, n_features)
+        :return: Predicted class labels for samples in X; array of shape (n_samples,)
+        """
+        # TODO: compute the predicted labels (+1 or -1)
+        y_pred = None
+
+        return y_pred
